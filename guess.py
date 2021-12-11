3da200b2344c8dd9fe2ef2cec38c840ce5cbf55a
Hardik Rusiya <91973842+hardiii13@users.noreply.github.com>
2021-12-11 10:07:04 +0530

    Create guess.py

---
+++ guess.py
@@ -0,0 +1,11 @@
+import random
+no_to_guess=random.randrange(0,201)
+#it will store the no to be guessed
+guessed_no=int(input("please enter you guess:"))
+if guessed_n0==no_to_guess:
+  print("your guess is correct")
+elif guessed_no<no_to_guess:
+  print("Your guessed no is too low")
+else:
+  print("your guessed number is too high")
+  
